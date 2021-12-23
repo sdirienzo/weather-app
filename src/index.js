5dbb4856f40835b2663c140fbdd7945063ffb7b1
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-12-23 13:22:51 -0500

    Add pubsub-js dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import PubSub from "pubsub-js";
 import AppController from "./controllers/app-controller";
 import AppView from "./views/app-view";
 import './styles/app-style.css';
 
-const app = new AppController(new AppView());
+const pubSub = PubSub;
+const app = new AppController(new AppView(pubSub));
 app.init();
