ebe367edf08332b1444aa4a2b98effdb67fc5bca
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-12-23 12:37:10 -0500

    Add new keyword before AppController instantiation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import AppController from "./controllers/app-controller";
 import AppView from "./views/app-view";
 import './styles/app-style.css';
 
-const app = AppController(new AppView());
+const app = new AppController(new AppView());
