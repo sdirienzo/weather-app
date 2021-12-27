ac3f466f4e1f3c33b1d8d072e63f42564367864c
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-12-27 12:34:59 -0500

    Add prepareCurrentWeatherPayload method to extract required values from currentWeatherResponse into a smaller object to be published

--- src/models/app-model.js
+++ src/models/app-model.js
@@ -22,6 +22,31 @@ class AppModel {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${this.apiKey}`, { mode: 'cors' });
         return response.json();
     }
+
+    prepareCurrentWeatherPayload(response) {
+        const currentWeather = {};
+        currentWeather.id = response.weather[0].id;
+        currentWeather.location = `${response.name}, ${response.sys.country}`;
+        currentWeather.condition = response.weather[0].main;
+        currentWeather.temp = Math.round(response.main.temp);
+        currentWeather.feelsLiketemp = Math.round(response.main.feels_like);
+        currentWeather.windSpeed = Math.round(response.wind.speed);
+        currentWeather.humidity = response.main.humidity;
+
+        return currentWeather;
+    }
+
+    async getWeather(msg, searchData) {
+        const currentWeather = {};
+        const weatherForecast = {};
+
+        const currentWeatherResponse = await this.getCurrentWeather(searchData.city);
+        const weatherForecastResponse = await this.getWeatherForecast(currentWeatherResponse.coord.lat, currentWeatherResponse.coord.lon);
+
+        
+        console.log(this.prepareCurrentWeatherPayload(currentWeatherResponse));
+        console.log(weatherForecastResponse);
+    }
 }
 
 export default AppModel;
